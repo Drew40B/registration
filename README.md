e7670edff4aef789b78d687bd0cb49ad35d93e8e
John Doe <johndoe@example.com>
2019-02-17 07:07:13 -0500

    Get pannel working

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # registration
-sample registration micro services
+Sample registration micro services
+
+## Panel
+Microservice for pannels. 
+* written in C# (.net core 2.2.2)
+* Port: 5001
+* OpenApi: http://localhost:5000/swagger/v1/swagger.json
--- pannel/pannel.api/Controllers/PannelController.cs
+++ pannel/pannel.api/Controllers/PannelController.cs
@@ -11,7 +11,6 @@ namespace pannel.api.Controllers
     public class PannelController : ControllerBase
     {
 
-
         private List<Panel> _pannels;
 
         public PannelController()
@@ -26,8 +25,19 @@ namespace pannel.api.Controllers
                 shortDescriptiom = "Disuccion on why pannels are so important",
                 longDescription = "Some realy long description on the advent of the study of panels",
                 startDateTime = new DateTime(DateTime.Now.Year, 01, 20, 9, 0, 0),
-                endDateTime = new DateTime(DateTime.Now.Year, 01, 20, 9, 0, 0)
+                endDateTime = new DateTime(DateTime.Now.Year, 01, 20, 10, 0, 0)
             });
+
+            _pannels.Add(new Panel()
+            {
+                pannelId = 2,
+                title = "Pannel-ology 2",
+                shortDescriptiom = "Continuation of disuccion on why pannels are so important",
+                longDescription = "Some realy long description on the advent of the study of panels",
+                startDateTime = new DateTime(DateTime.Now.Year, 01, 20, 10, 0, 0),
+                endDateTime = new DateTime(DateTime.Now.Year, 01, 20, 11, 0, 0)
+            });
+
 
         }
 
@@ -35,13 +45,32 @@ namespace pannel.api.Controllers
         /// Lists all pannels
         /// </summary>
         /// <returns>Array of pannels</returns>
-        [HttpGet]
+        [HttpGet()]
         [ProducesResponseType(typeof(PannelListEntry[]), 200)]
-
-
         async public Task<IActionResult> list()
         {
             return Ok(_pannels.ToArray());
+        }
+
+        /// <summary>
+        /// Gets a specific panel
+        /// </summary>
+        /// <param name="id">id of the panel to find</param>
+        /// <returns>pannel or not found</returns>
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Panel[]), 200)]
+        [ProducesResponseType(404)]
+        async public Task<IActionResult> get(int id)
+        {
+            var panel = _pannels.Find((entry) => entry.pannelId == id);
+
+            if (panel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(panel);
         }
     }
 }
